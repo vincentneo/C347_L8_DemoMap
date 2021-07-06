f5e3d5d9e8d83b4287775f9b966c8cdafdd113d5
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-07-06 11:34:32 +0800

    up to section D, changeable map type

--- app/src/main/java/sg/edu/rp/c347/id19007966/demomap/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/demomap/MainActivity.java
@@ -1,14 +1,63 @@
 package sg.edu.rp.c347.id19007966.demomap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonNormal, buttonTerrain, buttonSatellite;
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        SupportMapFragment mapFragment = (SupportMapFragment) fragmentManager.findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+            }
+        });
+
+        buttonNormal = findViewById(R.id.buttonNormal);
+        buttonTerrain = findViewById(R.id.buttonTerrain);
+        buttonSatellite = findViewById(R.id.buttonSatellite);
+
+        buttonNormal.setOnClickListener(this::onClickOfBtn);
+        buttonTerrain.setOnClickListener(this::onClickOfBtn);
+        buttonSatellite.setOnClickListener(this::onClickOfBtn);
+    }
+
+    public void onClickOfBtn(View btn) {
+        int id = btn.getId();
+        int mapTypeId;
+        switch (id) {
+            case R.id.buttonNormal:
+                mapTypeId = GoogleMap.MAP_TYPE_NORMAL;
+                break;
+            case R.id.buttonTerrain:
+                mapTypeId = GoogleMap.MAP_TYPE_TERRAIN;
+                break;
+            case R.id.buttonSatellite:
+                mapTypeId = GoogleMap.MAP_TYPE_SATELLITE;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected button id: " + id);
+        }
+        if (map != null) {
+            map.setMapType(mapTypeId);
+        }
     }
 }
